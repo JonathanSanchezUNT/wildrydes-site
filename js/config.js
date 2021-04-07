d2962455f188b5f7d3e190037ac7c204e20f6b28
Jonathan Sanchez <36934462+JonathanSanchezUNT@users.noreply.github.com>
2021-04-07 16:00:56 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_RWARkrJ5J', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5dtvplmt5kbvhlbj9f98gun1s9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
