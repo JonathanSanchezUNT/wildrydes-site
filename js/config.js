cc9ab16371c0b3463c849673d35e4ec3bcc6f1c6
Jonathan Sanchez <36934462+JonathanSanchezUNT@users.noreply.github.com>
2021-04-07 16:04:03 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_RWARkrJ5J', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5dtvplmt5kbvhlbj9f98gun1s9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
